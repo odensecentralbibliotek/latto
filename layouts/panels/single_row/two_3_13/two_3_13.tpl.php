a32bef754231829a5120e06a1e2c481e14eae3d8
Kristian Kaa <kaakristian@gmail.com>
2012-07-17 16:09:52 +0200

    panel layout 3-13

---
+++ layouts/panels/single_row/two_3_13/two_3_13.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Latto implementation to present a Panels layout.
+ *
+ * Available variables:
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout.
+ * - $css_id: unique id if present.
+ */
+?>
+<div class="grid-row">
+  <div class="grid-13-right">
+    <div class="grid-inner"><?php print $content['grid_13_right']; ?></div>
+  </div>
+  <aside class="grid-3-left">
+    <div class="grid-inner"><?php print $content['grid_3_left']; ?></div>
+  </aside>
+</div>
