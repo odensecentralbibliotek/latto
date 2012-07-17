87dfd198f81a7f506d5d12039b5a8e3864bdd5af
Kristian Kaa <kaakristian@gmail.com>
2012-07-17 16:36:22 +0200

    panels layout 12-4

---
+++ layouts/panels/single_row/two_12_4/two_12_4.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Latto implementation to present a Panels layout.
+ *
+ * Available variables:
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout.
+ * - $css_id: unique id if present.
+ */
+?>
+<div class="grid-row<?php print $css_id ? " $css_id" : ''; ?>">
+  <div class="grid-12-left">
+    <div class="grid-inner"><?php print $content['grid_12_left']; ?></div>
+  </div>
+  <aside class="grid-4-right">
+    <div class="grid-inner"><?php print $content['grid_4_right']; ?></div>
+  </aside>
+</div>
