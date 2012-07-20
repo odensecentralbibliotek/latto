be6d9150e6925229838ecf41a01952d8be49d89a
Brian Jensen <mail@magnify.dk>
2012-07-20 10:47:01 +0200

    Added template.php

---
+++ template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
+ * 1. Rename each function and instance of "adaptivetheme_subtheme" to match
+ *    your subthemes name, e.g. if your theme name is "footheme" then the function
+ *    name will be "footheme_preprocess_hook". Tip - you can search/replace
+ *    on "adaptivetheme_subtheme".
+ * 2. Uncomment the required function to use.
+ * 3. Read carefully, especially within adaptivetheme_subtheme_preprocess_html(), there
+ *    are extra goodies you might want to leverage such as a very simple way of adding
+ *    stylesheets for Internet Explorer and a browser detection script to add body classes.
+ */
+
