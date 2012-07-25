71b031811544d02fcbd081c1839f9af0aabceb18
Brian Jensen <mail@magnify.dk>
2012-07-25 09:36:30 +0200

    Fixed <li> that was printet in HTML in theme settings

--- theme-settings.php
+++ theme-settings.php
@@ -9,18 +9,18 @@ function latto_form_system_theme_settings_alter(&$form, $form_state) {
    * CSS class and markup
    */
   $form['classes'] = array(
-      '#type'          => 'fieldset',
-      '#title'         => t('Classes & Markup'),
-      '#description'   => t('Modify the default classes and markup from Drupal.'),
-      '#collapsible' => TRUE,
-      '#collapsed' => FALSE,
-      '#weight'=> -11
+    '#type'          => 'fieldset',
+    '#title'         => t('Classes & Markup'),
+    '#description'   => t('Modify the default classes and markup from Drupal.'),
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+    '#weight'=> -11
   );
   
   $form['classes']['menu'] = array(
     '#type'          => 'fieldset',
     '#title'         => t('Menus'),
-   '#description'   => t('Removes classes from the <li> tag in the menu.'),
+   '#description'   => t('Removes classes from the &lt;li&gt; tag in the menu.'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
