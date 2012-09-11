d2af2a8d725909ad5796d2c015f672b22d316411
Kristian Kaa <kaakristian@gmail.com>
2012-09-11 14:38:57 +0200

    Updated documentation

--- scripts/latto.js
+++ scripts/latto.js
@@ -1,6 +1,7 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * @file
+ * A JavaScript file to hold the common functions of the Latto theme.
+ * 
  */
 (function($) {
   $(document).ready(function () {
