9542b08d3d4b9bf0dec44675dcce885acf9e0317
Brian Jensen <mail@magnify.dk>
2012-09-14 12:29:08 +0200

    Added comments to example js

--- scripts/latto.js
+++ scripts/latto.js
@@ -5,8 +5,16 @@
  */
 (function($) {
   $(document).ready(function () {
+    
+    // Add example to search form input.
+    // 
+    // TODO: Optimize the selector so we don't rely on an id.
     $('#edit-search-block-form--2').example(function() {
       return $(this).attr('title');
     });
+    
+    
+    
+    
   });
 })(jQuery);
