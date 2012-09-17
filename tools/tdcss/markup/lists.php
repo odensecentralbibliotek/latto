867dc2538fc492064b1c48340dbd8b8c792d58a2
Brian Jensen <mail@magnify.dk>
2012-09-17 11:30:00 +0200

    Added date-list styling to tdcss

--- tools/tdcss/markup/lists.php
+++ tools/tdcss/markup/lists.php
@@ -108,6 +108,134 @@
   </li>
 </ul>
 
+<!-- : LIST UL NO ICONS (date-list) -->
+<ul class="list date-list">
+  <li class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p>Sted: Biblioteket</p>
+      <p>Tidspunkt: 10.00 - 11.00</p>
+      <p>Pris: Kr. 20,-</p>
+    </div>
+  </li>
+  <li class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p>Sted: Biblioteket</p>
+      <p>Tidspunkt: 10.00 - 11.00</p>
+      <p>Pris: Kr. 20,-</p>
+    </div>
+  </li>
+  <li class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p>Sted: Biblioteket</p>
+      <p>Tidspunkt: 10.00 - 11.00</p>
+      <p>Pris: Kr. 20,-</p>
+    </div>
+  </li>
+  <li class="list-item last">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p>Sted: Biblioteket</p>
+      <p>Tidspunkt: 10.00 - 11.00</p>
+      <p>Pris: Kr. 20,-</p>
+    </div>
+  </li>
+</ul>
+
+<!-- : LIST DIV WITH ICONS (date-list) -->
+<div class="list date-list">
+  <div class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p><i class="icon-home"></i> Biblioteket</p>
+      <p><i class="icon-time"></i> 10.00 - 11.00</p>
+      <p><i class="icon-credit-card"></i> Kr. 20,-</p>
+    </div>
+  </div>
+  <div class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p><i class="icon-home"></i> Biblioteket</p>
+      <p><i class="icon-time"></i> 10.00 - 11.00</p>
+      <p><i class="icon-credit-card"></i> Kr. 20,-</p>
+    </div>
+  </div>
+  <div class="list-item">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p><i class="icon-home"></i> Biblioteket</p>
+      <p><i class="icon-time"></i> 10.00 - 11.00</p>
+      <p><i class="icon-credit-card"></i> Kr. 20,-</p>
+    </div>
+  </div>
+  <div class="list-item last">
+    <h3 class="heading"><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</a></h3>
+    <div class="date-wrapper">
+      <div class="day">Onsdag</div>
+      <div class="date-wrapper-inner">
+	      <div class="date">8</div>
+        <div class="month">AUG</div>
+      </div>
+    </div>
+    <div class="content">      
+      <p><i class="icon-home"></i> Biblioteket</p>
+      <p><i class="icon-time"></i> 10.00 - 11.00</p>
+      <p><i class="icon-credit-card"></i> Kr. 20,-</p>
+    </div>
+  </div>
+</div>
+
 <!-- : LIST DIV TAG (floated) -->
 <div class="list floated">
   <div class="list-item">
