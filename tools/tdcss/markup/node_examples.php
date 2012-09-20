3e8154605ac7d135740ae1ff1266bd67d1109551
Kristian Kaa <kaakristian@gmail.com>
2012-09-20 12:02:49 +0200

    Updated CSS guide with node news display

--- tools/tdcss/markup/node_examples.php
+++ tools/tdcss/markup/node_examples.php
@@ -40,3 +40,54 @@
     versions of Lorem Ipsum.</p>
   </div>
 </div>
+
+<!-- : Node Type 1 -->
+<div class="node-type-1">
+  <div class="image-container">
+    <img alt="Et auto-skaleret billede" src="http://placehold.it/561x200&text=561x200" />
+  </div>
+  <div class="super-heading">
+    <a class="category label label-info" href="#">Category</a>
+    <a class="library" href="#">Library</a>
+    <span class="comment pull-right">
+      <i class="icon-comment"></i>
+      <span>(2)</span>
+    </span>
+  </div>
+  <h2 class="heading">This is new</h2>
+  <div class="lead">Uddrag: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia faucibus turpis, vitae tincidunt mi elementum sed.</div>
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia faucibus turpis, vitae tincidunt mi elementum sed. Proin consequat, quam id porttitor varius, nibh libero pulvinar ante, ac pellentesque massa elit eget est. In hac habitasse platea dictumst.
+    Integer non nunc in odio scelerisque euismod. Sed egestas orci et eros congue sed hendrerit purus blandit. Suspendisse est turpis, tristique vel fringilla sed, dictum vitae sem.
+
+    Pellentesque sit amet turpis est. Nullam vel turpis lorem, vel auctor libero. Pellentesque ac quam elit, in vulputate nisl. Fusce eu imperdiet tellus. Mauris non magna elit, eu adipiscing diam.
+    Nam nam
+    Nam dignissim, odio vitae ultricies adipiscing, massa purus aliquet est, nec sollicitudin odio nunc volutpat massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut id nulla at est rhoncus convallis
+  </div>
+  <div class="related">
+    <h3 class="heading">Se ogsaa: <span>(Relaterede artikler)</span></h3>
+    <a class="item" href="#">Integer non nunc in odio scelerisque</a>
+    <a class="item" href="#">Italia terra est</a>
+    <a class="item" href="#">Sicilia insula est</a>
+    <a class="item" href="#">Lec tiles kulitorum femihverum</a>
+  </div>
+  <div class="social">
+    Social icons goes here!
+  </div>
+  <div class="post-script">
+    <div class="image-container">
+      <img alt="Et auto-skaleret billede" src="http://placehold.it/100x100&text=100x100" />
+    </div>
+    <div class="content">
+      <span class="author">Af: <a href="#">Bent Betjentsen</a></span>
+      <span class="published">Udgivet 6. juni 2012</span>
+      <span class="updated">Opdateret 10. juni 2012</span>
+      <span class="tags">
+        Emner: 
+        <a href="#">Dolor sit amet</a>, 
+        <a href="#">Nam dignissim</a>, 
+        <a href="#">Tempor</a>
+      </span>
+    </div>
+  </div>
+</div>
