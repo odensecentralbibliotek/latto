30d8a6322ef7fa5e556f7f633d419b5c4db19d5c
Brian Jensen <mail@magnify.dk>
2012-10-10 16:13:16 +0200

    Added head markup include and styling of frontpage mockup

--- tools/tdcss/pages/frontpage.php
+++ tools/tdcss/pages/frontpage.php
@@ -3,19 +3,14 @@
    * Assembled mockup page for Event, full page
    */
 ?>
-<html>
-<head>
-    <title>Ding2tal mockup page: Frontpage</title>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr">
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <link rel="stylesheet" href="src/tdcss.css" type="text/css" media="screen">
+  <title>Ding2tal mockup page: Frontpage</title>
 
-    <!-- Project CSS -->
-    <link rel="stylesheet" href="../../../css/latto.bootstrap.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="../../../css/latto.styles.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="../demo/style.css" type="text/css" media="screen">
-
-    <script type="text/javascript" src="../src/vendors/jquery.js"></script>
-    <script type="text/javascript" src="../src/tdcss.js"></script>
+  <?php include 'mockup_head.inc'; ?>
 
 </head>
 <body>
@@ -27,84 +22,106 @@
       <div class="panel-pane pane-pane-header" >
         <div class="pane-content">
           <div class="header-inner">
-            <div class="logo">
-                <a href="/da" title="Hjem" rel="home">
-                  <img src="http://ding2.dev/sites/all/themes/ddbasic/logo.png" alt="Hjem" />
-               </a>
-            </div>
             <div class="site-name">
-              <a href="/da" title="Hjem" rel="home">
-                ding2.dev
-              </a>
+              <a href="/" title="Home" rel="home">Biblioteksnavn</a>
             </div>
+            <div class="site-slogan">Her kan der evt. stå et slogan</div>
           </div>
         </div>
       </div>
     </div>
   </header>
-    
+
   <article class="grid-full">
     <div class="grid-inner">
-      
-      <div class="search-wrapper contextual-links-region panel-pane pane-block pane-search-form" >
+      <div class="nav-wrapper panel-pane pane-block pane-menu-block-1">
+        <ul class="main-menu nav nav-inline"><li class="first active" style="width: 236px;"><span><a class="active" title="" href="/">Forsiden</a></span></li>
+          <li style="width: 236px;"><span><a href="/events">Events</a></span></li>
+          <li style="width: 236px;"><span><a href="/libraries">Libraries</a></span></li>
+          <li style="width: 236px;"><span><a href="/news">News</a></span></li>
+          <li class="last" style="width: 236px;"><span><a href="/page/kan">Det kan du</a></span></li>
+        </ul>
+      </div>
+      <div class="search-wrapper panel-pane pane-block pane-search-form">
         <div class="search-inner-wrapper">
-          <form action="/da/events/daisy-day" method="post" id="search-block-form" accept-charset="UTF-8"><div>  <h2 class="element-invisible">Søgefelt</h2>
-            <div class="form-item form-type-textfield form-item-search-block-form">
-              <label class="element-invisible" for="edit-search-block-form--2">Søg </label>
-             <input title="Søg efter materialer fra biblioteket.." class="has-example form-text" type="text" id="edit-search-block-form--2" name="search_block_form" value="" size="15" maxlength="128" />
+          <form accept-charset="UTF-8" id="search-block-form" method="post" action="/">
+            <div>
+              <h2 class="element-invisible">Search form</h2>
+              <div class="form-item form-type-textfield form-item-search-block-form">
+                <label for="edit-search-block-form--2" class="element-invisible">Search</label>
+                <input type="text" maxlength="128" size="15" value="" name="search_block_form" id="edit-search-block-form--2" class="has-example form-text ui-autocomplete-input example" title="Søg efter materialer fra biblioteket.." autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true">
+              </div>
+              <div id="edit-actions" class="form-actions form-wrapper">
+                <input type="submit" value="Search" name="op" id="edit-submit" class="btn btn-large btn-info form-submit">
+              </div>
+              <input type="hidden" value="form-iEXKcmcap-BsYSQjt_cPnRNti2wZeNEsOASFNM1EoBM" name="form_build_id">
+              <input type="hidden" value="search_block_form" name="form_id">
+              <input type="hidden" value="" name="sort" id="controls_search_sort">
+              <input type="hidden" value="" name="size" id="controls_search_size">
             </div>
-            <div class="form-actions form-wrapper" id="edit-actions"><input class="btn btn-large btn-info form-submit" type="submit" id="edit-submit" name="op" value="Søg" /></div><input type="hidden" name="form_build_id" value="form-9O-FKcL3OernxUXdNeUKILa1BPrPTyDoUBX_QkehtU8" />
-            <input type="hidden" name="form_token" value="g0KHxzSfQZNav_NkHRoonpQWriMvPjSRu_bIHWxsUrY" />
-            <input type="hidden" name="form_id" value="search_block_form" />
-            <input id="controls_search_sort" type="hidden" name="sort" value="" />
-            <input id="controls_search_size" type="hidden" name="size" value="" />
-            </div>
-          </form>  
+          </form>
         </div>
       </div>
 
-      <div class="panel-pane pane-page-content" >
+      <div class="panel-pane pane-page-content">
+
         <div class="pane-content">
-          <div class="contextual-links-region">
-            <div  class="grid-row grid-content">
-              <div class="grid-10-center">
-                <?php include('../markup/node_examples/nodeex-3-nodetype-event.php');?>
-                <?php include('../markup/base_elements/hr-ruler-1.php');?>
-                <?php include('../markup/typography/typography-02-h2.php');?>
-                <?php include('../markup/lists/list-7-simple.php');?>
-                <?php include('../markup/base_elements/hr-ruler-1.php');?>
-                <?php include('../markup/miscellaneous/misc-01-signature.php');?>
-              </div>
-            </div>
-            
-            <aside class="grid-3-left">
-              <div class="grid-inner">
-                <div class="panel-pane pane-event-category-menu" >
-                  <div class="pane-content">
-                    <?php include('../markup/miscellaneous/misc-02-subject-list.php');?>
+          <div>
+
+            <div class="grid-row grid-content">
+              <div class="grid-8-left">
+                <div class="grid-inner">
+                  <div class="panel-pane pane-views pane-ding-news">
+
+                    <div class="pane-content">
+                      <?php include '../markup/lists/list-1-default.inc'; ?>
+                    </div>
+
                   </div>
-               </div>
-             </div>
-           </aside>
-           <aside class="grid-3-right">
-             <div class="grid-inner">
-               <?php include('../markup/lists/list-2-floated.php');?>
-             </div>
-           </aside>
+                </div>
+              </div>
+
+              <aside class="grid-4-center-right">
+                <div class="grid-inner">
+                  <div class="panel-pane pane-views pane-ding-news">
+
+                    <div class="pane-content">
+                      <?php include '../markup/lists/list-4-date-list.inc'; ?>
+                    </div>
+
+                  </div>
+                </div>
+              </aside>
+
+
+              <aside class="grid-4-right">
+                <div class="grid-inner">
+                  <div class="panel-pane pane-views pane-ding-news">
+
+                    <div class="pane-content">
+                      <?php include '../markup/lists/list-7-compact.inc'; ?>
+                    </div>
+
+                  </div>
+                </div>
+              </aside>
+
+            </div>
           </div>
-        </div>  
+        </div>
       </div>
-    
+
     </div>
+
   </article>
-    
+
 
   <div class="grid-full bottom">
     <div class="grid-inner">
 
     </div>
   </div>
-    
+
 </body>
 </html>
 
