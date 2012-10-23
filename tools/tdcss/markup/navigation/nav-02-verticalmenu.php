b9b57921e30a81763e5a56aae1f93e937a707c34
Kristian Kaa <kaakristian@gmail.com>
2012-10-23 10:15:40 +0200

    Added nav and vertical menu to styleguide. Changed the way style guide menu is rendered

---
+++ tools/tdcss/markup/navigation/nav-02-verticalmenu.php
@@ -0,0 +1,8 @@
+<!-- : VERTICAL MENU -->
+
+<ul class="list v-menu">
+  <li class="item first"><a href="#" class="active">All Links</a></li>
+  <li class="item"><a href="#">Link 1</a></li>
+  <li class="item"><a href="#">Link 2</a></li>
+  <li class="item last"><a href="#">Link 3</a></li>
+</ul>
