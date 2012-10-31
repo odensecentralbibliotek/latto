780246b522aaade1e4a31628304c97a36ccdd835
Brian Jensen <mail@magnify.dk>
2012-10-31 16:31:08 +0100

    Fixed errors, cleaned up unused styling and optimized variables

--- tools/tdcss/pages/mq-test.php
+++ tools/tdcss/pages/mq-test.php
@@ -30,7 +30,7 @@
           <div class="content-inner">tertiary-content</div>
         </div>      
 
-        <div class="content-attachments">
+        <div class="attachments-wrapper">
 
           <div class="attachment-first">
             <div class="content-inner">attachment-first</div>
@@ -55,7 +55,7 @@
     
     <br /><hr /><br />
 
-    <h2>Template 1</h2>
+    <h2>Template 2</h2>
     <div class="grid-wrapper layout-template-two">    
       <div class="grid-inner">
 
@@ -71,7 +71,7 @@
           <div class="content-inner">tertiary-content</div>
         </div>      
 
-        <div class="content-attachments">
+        <div class="attachments-wrapper">
 
           <div class="attachment-first">
             <div class="content-inner">attachment-first</div>
@@ -97,35 +97,76 @@
     <br /><hr /><br />    
     
     <h2>Template 3</h2>
-    <div class="grid-wrapper layout-tpl-third">    
+    <div class="grid-wrapper layout-template-three">    
       <div class="grid-inner">
 
         <div class="primary-content">
-          primary-content
+          <div class="content-inner">primary-content</div>
         </div>
 
         <div class="secondary-content">
-          secondary-content
+          <div class="content-inner">secondary-content</div>
         </div>
 
         <div class="tertiary-content">
-          tertiary-content
-        </div>      
+          <div class="content-inner">tertiary-content</div>
+        </div>    
 
-        <div class="content-attachments">
+        <div class="attachments-wrapper">
 
           <div class="attachment-first">
-            attachment-first
+            <div class="content-inner">attachment-first</div>
           </div>
 
           <div class="attachment-second">
-            attachment-second
+            <div class="content-inner">attachment-second</div>
           </div>
 
           <div class="attachment-third">
-            attachment-third
+            <div class="content-inner">attachment-third</div>
           </div>
 
+        </div>
+
+      </div>
+    </div>
+    
+    <br /><hr /><br />    
+    
+    <h2>Template 4</h2>
+    <div class="grid-wrapper layout-template-three">    
+      <div class="grid-inner">
+
+        <div class="primary-content">
+          <div class="content-inner">primary-content</div>
+        </div>
+
+        <div class="secondary-content">
+          <div class="content-inner">secondary-content</div>
+        </div>
+
+        <div class="tertiary-content">
+          <div class="content-inner">tertiary-content</div>
+        </div>    
+
+        <div class="attachments-wrapper">
+
+          <div class="attachment-first">
+            <div class="content-inner">attachment-first</div>
+          </div>
+
+          <div class="attachment-second">
+            <div class="content-inner">attachment-second</div>
+          </div>
+
+          <div class="attachment-third">
+            <div class="content-inner">attachment-third</div>
+          </div>
+
+          <div class="attachment-fourth">
+            <div class="content-inner">attachment-fourth</div>
+          </div>
+          
         </div>
 
       </div>
