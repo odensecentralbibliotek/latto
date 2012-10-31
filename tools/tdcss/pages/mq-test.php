d46b83b1427389625273fa1f9e359248511334d4
Brian Jensen <mail@magnify.dk>
2012-10-31 15:12:41 +0100

    Added new grid styling with media queries and test page

---
+++ tools/tdcss/pages/mq-test.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr">
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+  <title>Grid test</title>
+
+  <?php include 'includes/mockup_head.inc'; ?>
+  <?php include 'includes/scripts.inc'; ?>
+</head>
+<body>
+   
+  <br /><br />
+  
+  <div class="grid-inner">
+      
+    <h2>Template 1</h2>
+    <div class="grid-wrapper layout-template-default">    
+      <div class="grid-inner">
+
+        <div class="primary-content">
+          <div class="content-inner">primary-content</div>
+        </div>
+
+        <div class="secondary-content">
+          <div class="content-inner">secondary-content</div>
+        </div>
+
+        <div class="tertiary-content">
+          <div class="content-inner">tertiary-content</div>
+        </div>      
+
+        <div class="content-attachments">
+
+          <div class="attachment-first">
+            <div class="content-inner">attachment-first</div>
+          </div>
+
+          <div class="attachment-second">
+            <div class="content-inner">attachment-second</div>
+          </div>
+
+          <div class="attachment-third">
+            <div class="content-inner">attachment-third</div>
+          </div>
+
+          <div class="attachment-fourth">
+            <div class="content-inner">attachment-fourth</div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    
+    <br /><hr /><br />
+
+    <h2>Template 1</h2>
+    <div class="grid-wrapper layout-template-two">    
+      <div class="grid-inner">
+
+        <div class="primary-content">
+          <div class="content-inner">primary-content</div>
+        </div>
+
+        <div class="secondary-content">
+          <div class="content-inner">secondary-content</div>
+        </div>
+
+        <div class="tertiary-content">
+          <div class="content-inner">tertiary-content</div>
+        </div>      
+
+        <div class="content-attachments">
+
+          <div class="attachment-first">
+            <div class="content-inner">attachment-first</div>
+          </div>
+
+          <div class="attachment-second">
+            <div class="content-inner">attachment-second</div>
+          </div>
+
+          <div class="attachment-third">
+            <div class="content-inner">attachment-third</div>
+          </div>
+
+          <div class="attachment-fourth">
+            <div class="content-inner">attachment-fourth</div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    
+    <br /><hr /><br />    
+    
+    <h2>Template 3</h2>
+    <div class="grid-wrapper layout-tpl-third">    
+      <div class="grid-inner">
+
+        <div class="primary-content">
+          primary-content
+        </div>
+
+        <div class="secondary-content">
+          secondary-content
+        </div>
+
+        <div class="tertiary-content">
+          tertiary-content
+        </div>      
+
+        <div class="content-attachments">
+
+          <div class="attachment-first">
+            attachment-first
+          </div>
+
+          <div class="attachment-second">
+            attachment-second
+          </div>
+
+          <div class="attachment-third">
+            attachment-third
+          </div>
+
+        </div>
+
+      </div>
+    </div>    
+    
+  </div>
+  
+</body>
+</html>  
